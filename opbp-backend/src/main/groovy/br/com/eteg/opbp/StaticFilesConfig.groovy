b76f7d988b6c0d7a2949ee6cd050002b2922a34f
dougefr <dougefr@gmail.com>
2017-04-15 17:25:05 -0300

    Ajustando webjar

---
+++ opbp-backend/src/main/groovy/br/com/eteg/opbp/StaticFilesConfig.groovy
@@ -0,0 +1,23 @@
+package br.com.eteg.opbp
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+class StaticFilesConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/")
+				.setViewName("forward:/app/index.html")
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/app/**")
+				.addResourceLocations(
+				"classpath:/META-INF/resources/webjars/opbp-frontend/0.0.1/")
+	}
+}
--- opbp-frontend/config/index.js
+++ opbp-frontend/config/index.js
@@ -10,7 +10,7 @@ module.exports = {
     index: path.resolve(__dirname, webjarFolder + '/index.html'),
     assetsRoot: webjarFolder,
     assetsSubDirectory: 'static',
-    assetsPublicPath: ['/', 'webjars/', packageJSON.name, '/', packageJSON.version, '/'].join(''),
+    assetsPublicPath: '/app',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
