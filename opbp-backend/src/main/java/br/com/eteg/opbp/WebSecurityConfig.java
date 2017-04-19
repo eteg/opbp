d804a27455717d77fda3961be53e1bfaf906b81c
dougefr <dougefr@gmail.com>
2017-04-18 22:34:34 -0300

    Removendo Basic Auth

--- opbp-backend/src/main/java/br/com/eteg/opbp/WebSecurityConfig.java
+++ opbp-backend/src/main/java/br/com/eteg/opbp/WebSecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/rest/**").fullyAuthenticated().and().httpBasic().and()
-        .csrf().disable();
+    http.authorizeRequests().antMatchers("/rest/**").fullyAuthenticated().and().csrf().disable();
   }
 }
