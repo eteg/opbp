8a9767a0e3cd8f4c3d5a155ae590201425cfe91c
dougefr <dougefr@gmail.com>
2017-04-18 22:36:11 -0300

    Implementando fluxo de login

--- opbp-frontend/app/src/store/auth/actions.js
+++ opbp-frontend/app/src/store/auth/actions.js
@@ -2,16 +2,19 @@ import Vue from 'vue'
 import * as types from './mutation-types'
 
 export const signIn = ({ commit }, { username, password }) => {
-  let data = new FormData()
-  data.append('action', 'login')
-  data.append('username', username)
-  data.append('password', password)
+  let loginFormData = new FormData()
+  loginFormData.append('action', 'login')
+  loginFormData.append('username', username)
+  loginFormData.append('password', password)
 
-  Vue.axios.post('login', data,
+  Vue.axios.post('login', loginFormData,
     {
       headers: { 'Content-Type': 'multipart/form-data' }
     }).then((response) => {
-      console.log(response)
+      return Vue.axios.get('rest/accounts/search/findByUsername', {
+        params: { username }
+      })
+    }).then((response) => {
       commit(types.SIGN_IN, response.data)
     })
 }
