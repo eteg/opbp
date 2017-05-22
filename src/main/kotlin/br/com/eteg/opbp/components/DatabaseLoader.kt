3b799754f4897791af40fb956212cd45c01631bf
dougefr <douglas.rodrigues@eteg.com.br>
2017-05-21 21:32:36 -0300

    Formatando código

--- src/main/kotlin/br/com/eteg/opbp/components/DatabaseLoader.kt
+++ src/main/kotlin/br/com/eteg/opbp/components/DatabaseLoader.kt
@@ -3,14 +3,13 @@ package br.com.eteg.opbp.components
 import br.com.eteg.opbp.entities.Account
 import br.com.eteg.opbp.repositories.AccountRepository
 import org.springframework.boot.CommandLineRunner
-import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Component
 class DatabaseLoader(val userRepository: AccountRepository) : CommandLineRunner {
     override fun run(vararg args: String?) {
         with(userRepository) {
-            if(count() == 0L) {
+            if (count() == 0L) {
                 save(Account(username = "admin", password = "admin",
                         name = "Admin", email = "-"))
             }
